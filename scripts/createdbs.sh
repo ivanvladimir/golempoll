bc0ca8bd6fd781c17b3e95509c8ff43068e317fe
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2014-08-12 16:38:52 -0500

    Getting ready sessions

---
+++ scripts/createdbs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p 'Do you want to create users db?' -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	echo "{}" > users.yaml
+fi
+
+read -p 'Do you want to create experiments db?' -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	echo "{}" > experiments.yaml
+fi
