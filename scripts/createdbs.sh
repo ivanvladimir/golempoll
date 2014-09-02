738b529c966deafade986238ad1c32a66e9a2ad8
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2014-09-01 23:39:18 -0500

    Fixing the scripts for database

--- scripts/createdbs.sh
+++ scripts/createdbs.sh
@@ -2,14 +2,14 @@
 
 read -p 'Do you want to create users db? ' -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo "{}" > users.yaml
 fi
 
 read -p 'Do you want to create experiments db? ' -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo "{}" > experiments.yaml
 fi
